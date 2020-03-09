74d00bd940a9b994d54c59af1ec9885436bdeca0
ikroop <virkikroop13@gmail.com>
2020-03-09 18:15:13 -0400

    [ikroop] class car created

---
+++ Group7_W2020_MAD3115_FP/Vehicle/Car.swift
@@ -0,0 +1,13 @@
+//
+//  Car.swift
+//  Group7_W2020_MAD3115_FP
+//
+//  Created by MacStudent on 2020-03-09.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import Foundation
+class Car
+{
+    
+}
