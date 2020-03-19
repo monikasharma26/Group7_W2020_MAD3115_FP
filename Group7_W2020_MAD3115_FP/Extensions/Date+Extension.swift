c8822fb5607848d8ca7c32b5bd1e13f0982b9d31
ikroop <virkikroop13@gmail.com>
2020-03-18 22:46:46 -0400

    [Roop] Method from dAte added

--- Group7_W2020_MAD3115_FP/Extensions/Date+Extension.swift
+++ Group7_W2020_MAD3115_FP/Extensions/Date+Extension.swift
@@ -16,4 +16,12 @@ extension Date
         let formattedDate = dateFormatterPrint.string(from: self)
         return formattedDate
     }
+    public static func from(year: Int, month: Int, day: Int) -> Date? {
+           let calendar = Calendar(identifier: .gregorian)
+           var dateComponents = DateComponents()
+           dateComponents.year = year
+           dateComponents.month = month
+           dateComponents.day = day
+           return calendar.date(from: dateComponents) ?? nil
+       }
 }
