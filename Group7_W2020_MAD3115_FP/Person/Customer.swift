246a7f0464140b95d7b960cddd90b70dcb99a9bd
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-03-19 17:40:18 -0400

    [KRITIMA] Error handled for valid email and mobile number in customer class

--- Group7_W2020_MAD3115_FP/Person/Customer.swift
+++ Group7_W2020_MAD3115_FP/Person/Customer.swift
@@ -16,4 +16,19 @@ class Customer
      var amountToPayForAllRentedVehicles : Float = 0.0
     
      init(id: String, firstName: String, lastName: String, gender: String, birthDate: Int, mobileNumber: String, emailId: String, userName: String, password: String, address : String?, city: String?, vehicle : VehicleM?) throws {
+        
+        guard (emailId.isValidEmail()) else {
+                  throw CustomerError.emailInvalid
+              }
+              
+              guard( mobileNumber.isvalidPhoneNumber() ) else {
+                      throw CustomerError.mobileInvalid
+                  }
+             try super.init (id: id, firstName: firstName, lastName: lastName, gender: gender, birthDate: birthDate, mobileNumber: mobileNumber, emailId: emailId, userName: userName, password: password)
+              
+              self.address = address
+              self.city = city
+              self.vehicle = vehicle
+                 
+             }
 }
