10abd9cd79249fdb14cf28a6fd12d8aa164221c6
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-03-19 17:26:50 -0400

    [KRITIMA] Initializer added in owner class

--- Group7_W2020_MAD3115_FP/Person/Owner.swift
+++ Group7_W2020_MAD3115_FP/Person/Owner.swift
@@ -15,4 +15,18 @@ class Owner
     var webSite : String  = "www.lte.com"
 
       var vehicle : VehicleM?
+    
+    init(id: String, firstName: String, lastName: String, gender: String, birthDate: Int, mobileNumber: String, emailId: String, userName: String, password: String, address : String, city : String, vehicle : VehicleM?) throws {
+        
+       guard (emailId.isValidEmail()) else {
+           throw CustomerError.emailInvalid
+       }
+       guard( mobileNumber.isvalidPhoneNumber()) else {
+               throw CustomerError.mobileInvalid
+           }
+         try super.init(id: id, firstName: firstName, lastName: lastName, gender: gender, birthDate: birthDate, mobileNumber: mobileNumber, emailId: emailId, userName: userName, password: password)
+        
+        self.vehicle = vehicle
+        
+    }
 }
