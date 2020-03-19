df81f910fa995967092c2721ed63556890b8c884
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-03-19 14:02:52 -0400

    [KRITIMA] Appropriate function return call added

--- Group7_W2020_MAD3115_FP/Person/Person.swift
+++ Group7_W2020_MAD3115_FP/Person/Person.swift
@@ -7,32 +7,38 @@
 //
 enum Gender : String
 {
-   case Male
-   case Female
-   case Other
+    case Male
+    case Female
+    case Other
 }
 
 import Foundation
 class Person {
-
-var id : String
-           var firstName: String
-           var lastName:  String
-           var fullName: String {  get
-                           {
-                           return "\(firstName) \(lastName)"
-                           }
-           }
-           var gender :  String
-           var birthDate : Int?
-           var age: Int
-              {
-                  get
-                  {
-                      return Date.dateDifference(birthYear: self.birthDate!)
-                  }
-              }
     
-
+    var id : String
+    var firstName: String
+    var lastName:  String
+    var fullName: String {  get
+    {
+        return "\(firstName) \(lastName)"
+        }
+    }
+    var gender :  String
+    var birthDate : Int?
+    var age: Int
+    {
+        get
+        {
+            return Date.dateDifference(birthYear: self.birthDate!)
+        }
+    }
     
+    var mobileNumber : String
+    var emailId : String
+    var userName : String
+    var password : String
+    var vehicleDictionary : Dictionary<Int, Vehicle>?
+
+
+
 
