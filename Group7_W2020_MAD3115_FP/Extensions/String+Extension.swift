6739c03f612bbae2ff4ab5012837c36eab3e5fd2
ikroop <virkikroop13@gmail.com>
2020-03-18 22:58:55 -0400

    [roop] isvalidemail added

--- Group7_W2020_MAD3115_FP/Extensions/String+Extension.swift
+++ Group7_W2020_MAD3115_FP/Extensions/String+Extension.swift
@@ -27,4 +27,10 @@ extension String{
             print(error.localizedDescription)
         }
     }
+    func isValidEmail() -> Bool{
+         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+            let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+            return emailTest.evaluate(with: self)
+               }
+
 }
