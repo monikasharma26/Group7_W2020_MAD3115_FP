c0d14ba4620fad035a3cd35e1f9f0a422fb8e39c
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-03-19 13:10:31 -0400

    [KRITIMA] Structure added for vehicle model

--- Group7_W2020_MAD3115_FP/Models/VehicleM.swift
+++ Group7_W2020_MAD3115_FP/Models/VehicleM.swift
@@ -8,7 +8,15 @@
 
 import Foundation
 struct VehicleM : Codable {
-   
+    
+   let vehicleIdentificationNumber, vehicleDescription, manufacturerName: String
+      let isSelfDrive, isInsured: Bool
+      let noOfSeat: Int
+      let fuelType : String
+      let topSpeed: Int = 0
+      let mileage : Int = 0
+      var vehicleType : VehicleT?
+      let vehicleRent : VehicleRentM?
 }
 enum VehicleT: String, Codable {
     case car = "Car", motorcycle = "Motorcycle" , bus = "Bus"
