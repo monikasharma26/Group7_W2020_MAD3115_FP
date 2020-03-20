1e086a4c118006f879900c401e77f9851c1e0493
Monika <monikasharma26081991@gmail.com>
2020-03-20 00:02:14 -0400

    [Monika] UIColor Extension Method added

--- Group7_W2020_MAD3115_FP/Extensions/UIColor.swift
+++ Group7_W2020_MAD3115_FP/Extensions/UIColor.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+import UIKit
+extension UIColor
+{
+    class func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+    
+    class func pinkThemeColor() -> UIColor
+    {
+        return UIColor(red: 111/255, green: 202/255, blue: 238/255, alpha: 1)
+        
+    }
+    
+    class func rbg(r: CGFloat, g: CGFloat, b: CGFloat) -> UIColor {
+        let color = UIColor.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+        return color
+    }
+    
+}
