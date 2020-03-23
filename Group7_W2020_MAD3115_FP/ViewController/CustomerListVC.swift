478fe1406ee3112b8e7ccd4271ed261287d7772b
Monika <monikasharma26081991@gmail.com>
2020-03-22 20:18:16 -0400

    [Monika] CustomerList vc added

---
+++ Group7_W2020_MAD3115_FP/ViewController/CustomerListVC.swift
@@ -0,0 +1,30 @@
+//
+//  CustomerListVC.swift
+//  Group7_W2020_MAD3115_FP
+//
+//  Created by S@i on 2020-03-22.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import UIKit
+
+class CustomerListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
