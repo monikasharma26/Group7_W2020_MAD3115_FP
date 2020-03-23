712fdae482bcc0e1a318e6b90a852ace6d140c24
Monika <monikasharma26081991@gmail.com>
2020-03-23 00:14:02 -0400

    [Monika] CustomerListVc added

---
+++ Group7_W2020_MAD3115_FP/ViewController/CustomerListTVC.swift
@@ -0,0 +1,39 @@
+//
+//  CustomerListTVC.swift
+//  Group7_W2020_MAD3115_FP
+//
+//  Created by S@i on 2020-03-22.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import UIKit
+
+class CustomerListTVC: UITableViewCell {
+    
+    // MARK: - Properties
+    @IBOutlet var contan_view: UIView!
+    @IBOutlet var name_lbl: UILabel!
+    @IBOutlet var email_lbl: UILabel!
+    @IBOutlet var img_view: UIImageView!
+    
+    // MARK: - Life Cycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+       // contan_view.addBorder(view: contan_view, radius: 8.0, width: 1, color: UIColor.lightGray.cgColor)
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    // MARK: - Helper
+    func setDisplay(customer: CustomersVM) {
+        //
+        name_lbl.text = customer.fullName
+        email_lbl.text = customer.email ?? ""
+    }
+    
+
+}
